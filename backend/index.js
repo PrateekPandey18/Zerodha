97a7dc8465a94e614eab60b7aa56738a06593ac0
PrateekPandey18 <prateekpandey437@gmail.com>
2026-07-03 08:39:28 +0530

    api endpoints working

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const cors = require("cors")
 const {HoldingsModel} = require("./model/HoldingsModel")
 const {PositionsModel} = require("./model/PositionsModel")
 const PORT = process.env.PORT ||3002
 
 const app = express()
 const uri = process.env.MONGO_URL;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // app.get("/setup",async(req,res) =>{
 //     let tempHoldings = [
@@ -47,6 +52,14 @@ const uri = process.env.MONGO_URL;
 //   res.send("done")
 // })
 
+app.get("/allHoldings",async(req,res)=>{
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
+})
+app.get("/allPositions",async(req,res)=>{
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+})
 
 app.listen(PORT,()=>{
     console.log("app started")
--- dashboard/src/components/Positions.jsx
+++ dashboard/src/components/Positions.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 import { positions } from "../data/data";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Positions = () => {
+  const [allPostions,setAllPositions] = useState([])
+  useEffect(()=>{
+    axios.get("http://localhost:3002/allPositions").then((res)=>{
+      setAllPositions(res.data)
+      console.log(res.data);
+    })
+  },[])
   return (
+    
     <>
-      <h3 className="title">Positions ({positions.length})</h3>
+      <h3 className="title">Positions ({allPostions.length})</h3>
 
       <div className="order-table">
         
@@ -23,7 +34,7 @@ const Positions = () => {
           </tr>
           </thead>
           <tbody>
-          {positions.map((stock,index)=>{
+          {allPostions.map((stock,index)=>{
                       const currValue = stock.price *stock.qty 
                       const isProfit = currValue - stock.avg *stock.qty >=0.0;
                       const profClass = isProfit ? "profit":"loss";
